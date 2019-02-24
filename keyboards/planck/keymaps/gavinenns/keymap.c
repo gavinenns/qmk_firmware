644d8c89898696cc0f798d4377eb5fb185046d3e
Gavin Enns <gavin.enns@gmail.com>
2019-02-24 15:00:29 -0500

    adds super key bottom-left

--- keyboards/planck/keymaps/gavinenns/keymap.c
+++ keyboards/planck/keymaps/gavinenns/keymap.c
@@ -31,6 +31,7 @@ enum planck_layers {
 #define RAISE LT(_RAISE, KC_SPC)
 #define MED_ESC LT(_MEDIA, KC_ESC)
 #define MED_BSP LT(_MEDIA, KC_BSPC)
+#define SUPER LCTL(LALT(KC_LGUI))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -49,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   MED_BSP,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,   KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP,   KC_RSFT,
-    KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,  KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT ),
+    SUPER,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,  KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
