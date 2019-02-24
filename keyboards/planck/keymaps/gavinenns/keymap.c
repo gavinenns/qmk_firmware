64f410db152e950b09715cf55c1c5741558e546a
Gavin Enns <gavin.enns@gmail.com>
2019-02-24 14:43:46 -0500

    inverted t arrow/nav, rshift added, enter bottom row

--- keyboards/planck/keymaps/gavinenns/keymap.c
+++ keyboards/planck/keymaps/gavinenns/keymap.c
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    MED_BSP,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT ),
+    MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   MED_BSP,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,   KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP,   KC_RSFT,
+    KC_NO,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,  KC_ENT, KC_LEFT, KC_DOWN,   KC_RGHT ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  _______,   _______,   _______,   _______,   _______,   _______,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, _______,   _______,   _______,   _______,  _______,  _______,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
+    _______, _______,   _______,   _______,   _______,   _______,   _______,  S(KC_NUHS), S(KC_NUBS), _______, KC_PGUP,  _______,
+    _______, _______,   _______,   _______,   _______,   _______,   _______,   _______,    _______,    KC_HOME, KC_PGDN, KC_END
 ),
 
 /* Raise
@@ -83,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  _______,   _______,   _______,   _______,   _______,   _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______,   _______,   _______,   _______,   _______,   _______,   KC_NUHS, KC_NUBS, KC_SLSH, KC_QUES, _______,
+    _______, _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______, _______, _______
 ),
 
 [_MEDIA] = LAYOUT_planck_grid(
