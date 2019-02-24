4f52746716445dede5d3d2a2e4c58e93e8a22eab
Gavin Enns <gavin.enns@gmail.com>
2019-02-24 16:43:33 -0500

    adds normal bspc to raise

--- keyboards/planck/keymaps/gavinenns/keymap.c
+++ keyboards/planck/keymaps/gavinenns/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //  KC_NUHS, KC_NUBS    S(KC_NUHS), S(KC_NUBS)
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,     KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,     KC_P7,    KC_P8,    KC_P9,    KC_0,  KC_DEL,
+    KC_GRV,     KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,     KC_P7,    KC_P8,    KC_P9,    KC_0, KC_BSPC,
     _______, _______,   _______,   _______,   _______,   KC_PSLS,   KC_PAST,     KC_P4,    KC_P5,    KC_P6, KC_QUES, KC_SLSH,
     _______, _______,   _______,   _______,   _______,   KC_PMNS,   KC_PPLS,     KC_P1,    KC_P2,    KC_P3, KC_PEQL, KC_PENT,
     _______, _______,   _______,   _______,   _______,   _______,   _______,   _______,    KC_P0,  KC_PDOT, _______, _______
