b1e4f9a44dbcb7abf71490bcd3bc1dcc691f536b
Gavin Enns <gavin.enns@gmail.com>
2019-02-26 23:42:07 -0500

    adds non-shift enter key to game layer

--- keyboards/planck/keymaps/gavinenns/keymap.c
+++ keyboards/planck/keymaps/gavinenns/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,   MED_BSP,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_CMEX, KC_DTQS,    KC_UP,   KC_SFTENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_CMEX, KC_DTQS,    KC_UP,  KC_SFTENT,
     SUPER,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,    KC_RGUI, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
@@ -58,7 +58,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,   _______,   _______, _______,   _______,   _______,   _______,    _______,    KC_HOME, KC_PGDN, KC_END
 ),
 
-//  KC_NUHS, KC_NUBS    S(KC_NUHS), S(KC_NUBS)
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,     KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,     KC_P7,    KC_P8,    KC_P9,    KC_0, KC_BSPC,
     _______, _______,   _______,   _______,   _______,   KC_PSLS,   KC_PAST,     KC_P4,    KC_P5,    KC_P6, KC_PLUS, KC_SLSH,
@@ -84,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = LAYOUT_planck_grid(
   KC_ESC, _______, _______, _______,  _______, _______, _______, _______, _______,  _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_ENT,
   _______, _______, _______, _______,  KC_SPC,  KC_SPC, _______, _______, _______,  _______, _______, _______
 )
 
