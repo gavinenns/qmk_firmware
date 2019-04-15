2afcaaf85c22e21b29ac8ab9b3c6a22ac0338626
Gavin Enns <gavin.enns@gmail.com>
2019-04-15 18:14:23 -0400

    moves brackets, adjusts symbols around numpad

--- keyboards/planck/keymaps/gavinenns/keymap.c
+++ keyboards/planck/keymaps/gavinenns/keymap.c
@@ -57,24 +57,24 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-    MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,   MED_BSP,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,   KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_CMEX, KC_DTQS,    KC_UP,  KC_SFTENT,
-    SUPER,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,    KC_RGUI, KC_LEFT, KC_DOWN,   KC_RGHT
+    MED_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,       KC_P,  MED_BSP,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_CMEX, KC_DTQS,   KC_UP,  KC_SFTENT,
+    SUPER,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,  LOWER,   RAISE,   RAISE,   KC_RGUI, KC_LEFT, KC_DOWN,  KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM,     KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,   KC_CIRC,   KC_AMPR,    KC_ASTR,    KC_UNDS,  KC_MINS, KC_DEL,
-    _______, _______,   KC_LCBR,   KC_RCBR, KC_LPRN,   KC_RPRN,   _______,   _______,    _______,    KC_EQL,  KC_PLUS, KC_BSLS,
-    _______, _______,   KC_LBRC,   KC_RBRC, KC_LABK,   KC_RABK,   _______,   _______,    _______,    _______, KC_PGUP, _______,
-    _______, _______,   _______,   _______, _______,   _______,   _______,   _______,    _______,    KC_HOME, KC_PGDN, KC_END
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_MINS, KC_DEL,
+    _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, KC_PIPE, KC_BSLS,
+    _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, _______, _______, _______, _______, KC_SLSH, KC_PGUP, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
 ),
 
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,     KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,     KC_P7,    KC_P8,    KC_P9,    KC_0, KC_BSPC,
-    _______, _______,   _______,   _______,   _______,   KC_PSLS,   KC_PAST,     KC_P4,    KC_P5,    KC_P6, KC_PIPE, KC_SLSH,
-    _______, _______,   _______,   _______,   _______,   KC_PMNS,   KC_PPLS,     KC_P1,    KC_P2,    KC_P3, KC_PEQL, RS_PENT,
-    _______, _______,   _______,   _______,   _______,   _______,   _______,   _______,    GUI_P0, KC_PDOT, _______, KC_NLCK
+    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_P7,   KC_P8,    KC_P9,    KC_0, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,    KC_P6, KC_PAST, KC_PSLS,
+    _______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,    KC_P3, KC_PPLS, RS_PENT,
+    _______, _______, _______, _______, _______, _______, _______, _______,   GUI_P0, KC_PDOT, KC_PMNS, KC_PEQL
 ),
 
 [_MEDIA] = LAYOUT_planck_grid(
