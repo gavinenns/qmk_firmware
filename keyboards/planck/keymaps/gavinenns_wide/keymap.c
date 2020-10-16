fc2f8100c68a6d88a9840d59e278e3a7f88c10be
Gavin Enns <gavin.enns@gmail.com>
2020-10-16 19:26:01 -0400

    adds enter, backspace and delete to game layer

--- keyboards/planck/keymaps/gavinenns_wide/keymap.c
+++ keyboards/planck/keymaps/gavinenns_wide/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,  KC_F7,  KC_F8,  KC_F9, KC_F15,
     MOD_1,   MOD_2,   MOD_3,   MOD_4,   MOD_5, ___x___, ___x___, ___x___,  KC_F4,  KC_F5,  KC_F6, KC_F14,
   ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,  KC_F1,  KC_F2,  KC_F3, KC_F13,
-  _______, ___x___, _______,  KC_TAB, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT, KC_BSPC, KC_F10, KC_F11,  KC_F12
+  _______, ___x___, _______,  KC_TAB, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT, KC_BSPC, KC_F10, KC_F11, KC_F12
 ),
 
 [_ARROW] = LAYOUT_planck_grid(
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ___x___, ___x___, KC_PGUP, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,  ___x___, ___x___, ___x___,
   ___x___, KC_HOME, KC_PGDN,  KC_END, ___x___, ___x___, ___x___,   MOD_5,   MOD_4,    MOD_3,   MOD_2,   MOD_1,
   ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,  ___x___, ___x___, ___x___,
-  _______, ___x___,  KC_ESC,  KC_TAB,  KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT, _______,  KC_DEL, ___x___, _______
+  _______, ___x___,  KC_ESC,  KC_TAB,  KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT, _______,   KC_DEL, ___x___, _______
 ),
 
 [_BRACKET] = LAYOUT_planck_grid(
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,    KC_Q,    KC_W,     KC_E,    KC_R,    KC_3,    KC_6,    KC_7,   KC_8,     KC_9,    KC_0, ___x___,
    KC_TAB,    KC_A,    KC_S,     KC_D,    KC_F,    KC_2,    KC_5, ___x___, ___x___,  ___x___,   KC_UP, ___x___,
   KC_LSFT,    KC_Z,    KC_X,     KC_C,    KC_V,    KC_1,    KC_4, ___x___, ___x___,  KC_LEFT, KC_DOWN, KC_RIGHT,
-  KC_LCTL, KC_LCTL, KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC, ___x___, ___x___, ___x___,  ___x___, ___x___, _______
+  KC_LCTL, KC_LCTL, KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT, KC_BSPC,   KC_DEL, ___x___, _______
 ),
 
 [_LAYERLOCK] = LAYOUT_planck_grid(
