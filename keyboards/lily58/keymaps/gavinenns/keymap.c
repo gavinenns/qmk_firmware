48c502b9b4c639544a01a368f194c4bd0e869c1b
Gavin Enns <gavin.enns@gmail.com>
2021-04-03 13:57:00 -0400

    custom movie toggle, grave-esc, use 12 f keys

--- keyboards/lily58/keymaps/gavinenns/keymap.c
+++ keyboards/lily58/keymaps/gavinenns/keymap.c
@@ -8,16 +8,19 @@ enum layer_number {
   _ADJUST,
 };
 
+enum custom_keycodes {
+  KC_MOVI = SAFE_RANGE
+};
+
 #define KC_RAIS MO(_RAISE)
 #define KC_LOWR MO(_LOWER)
-#define KC_MOVI TG(_MOVIE)
 #define KC_BASE TO(_QWERTY)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BKSP |
+ * | ESC/`|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BKSP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | ENTR |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -36,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
 
 [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,  \
+  KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,  \
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_VOLD,  KC_VOLU,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, \
                       KC_LALT, KC_LGUI, KC_LOWR, KC_SPC,           KC_RSFT, KC_RAIS, KC_RALT, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  `   |      |      |  -   |  =   | DEL  |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |                    |  `   |      |      |  -   |  =   | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  F6  |  F7  |  F8  |  F9  | F10  |                    |      |      |      |  [   |  ]   |      |
+ * |      |  F5  |  F6  |  F7  |  F8  |      |                    |      |      |      |  [   |  ]   |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |  F11 |  F12 |  F13 |  F14 | F15  |-------.    ,-------|      |      |      |      |      |  \   |
+ * |      |  F9  |  F10 |  F11 |  F12 |      |-------.    ,-------|      |      |      |      |      |  \   |
  * |------+------+------+------+------+------| Mute  |    | Play  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -57,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                    KC_GRV, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL,  KC_DEL, \
-  _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
-  _______,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, KC_MUTE, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                    KC_GRV, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL,  KC_DEL, \
+  _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
+  _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
                              _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 /* RAISE
@@ -83,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP, XXXXXXX,                   KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_CIRC, KC_PENT, \
   _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,                   KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_LPRN, KC_RPRN, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_PPLS,   KC_P0,   KC_P0, KC_PDOT, KC_PEQL, _______, \
-                             _______, _______, _______, _______, _______, _______, _______, RGUI_T(KC_MOVI) \
+                             _______, _______, _______, _______, _______, _______, _______, KC_MOVI \
 ),
 /* Movie (maybe just make this put arrow keys on top. trouble is jupiter hell needs the dot too)
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -173,12 +176,32 @@ void oled_task_user(void) {
 }
 #endif // OLED_DRIVER_ENABLE
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint16_t movi_tap_timer;
+  static bool movi_inverting;
+
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
     set_keylog(keycode, record);
 #endif
+    if (keycode == KC_MOVI) {
+      movi_tap_timer = timer_read();
+      movi_inverting = true;
+      register_mods(MOD_BIT(KC_RALT));
+      return false;
+    }
     // set_timelog();
+  } else {
+    if (keycode == KC_MOVI) {
+      unregister_mods(MOD_BIT(KC_LALT));
+      if (timer_elapsed(movi_tap_timer) < TAPPING_TERM && movi_inverting) {
+        layer_on(_MOVIE);
+      }
+      return false;
+    }
   }
+
+  movi_inverting = false;
   return true;
 }
