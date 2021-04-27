e01c8b1117e527cbf17a377b9cc1877c8cab41b1
Gavin Enns <gavin.enns@gmail.com>
2021-04-27 09:44:37 -0400

    make alt/gui symetrical, enable audio keys

--- keyboards/lily58/keymaps/gavinenns/keymap.c
+++ keyboards/lily58/keymaps/gavinenns/keymap.c
@@ -18,6 +18,8 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+// check this out https://git.kdb424.xyz/kdb424/qmk-firmware/src/branch/kdb424/keyboards/crkbd/keymaps/kdb424/keymap.c
+
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC/`|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BKSP |
@@ -28,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| VolDn |    | VolUp |------+------+------+------+------+------|
  * | LCTRL|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | RCtl |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RAlt | RGUI |
+ *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -43,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,  \
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_VOLD,  KC_VOLU,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL, \
-                      KC_LALT, KC_LGUI, KC_LOWR, KC_SPC,           KC_RSFT, KC_RAIS, KC_RALT, KC_RGUI \
+                      KC_LALT, KC_LGUI, KC_LOWR, KC_SPC,           KC_RSFT, KC_RAIS, KC_RGUI, KC_RALT \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Mute  |    | Play  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RAlt | RGUI |
+ *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Prev  |    | Next  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   +  |  0   |  0   |  .   |  =   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RAlt | RGUI |
+ *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      | MOVI |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -98,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      | Left | Down | Right|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RAlt | RGUI |
+ *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -123,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RAlt | RGUI |
+ *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
