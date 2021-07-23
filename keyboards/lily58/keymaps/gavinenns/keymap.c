64e8d7842c7288f0357b8e483f606ba4a418bc1c
Gavin Enns <gavin.enns@gmail.com>
2021-07-22 21:36:33 -0400

    standard escape on lower for cmd-alt-esc, swap pad plus minus

--- keyboards/lily58/keymaps/gavinenns/keymap.c
+++ keyboards/lily58/keymaps/gavinenns/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                    KC_GRV, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL,  KC_DEL, \
+   KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                    KC_GRV, XXXXXXX, XXXXXXX, KC_MINS,  KC_EQL,  KC_DEL, \
   _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
   _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MMUT, KC_MMPL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
@@ -94,15 +94,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |-------|    |-------|   +  |  0   |  0   |  .   |  =   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | / Space /       \ RSft \  |RAISE | RGUI | RAlt |
- *                   |      |      |      |/       /         \      \ |      |      | MOVI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT( \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PSLS,   KC_P7,   KC_P8,   KC_P9,   KC_P0, _______, \
   _______, XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_PGUP,                   KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_CIRC, KC_PENT, \
-  _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_LPRN, KC_RPRN, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MMPR, KC_MMNX, KC_PPLS,   KC_P0,   KC_P0, KC_PDOT, KC_PEQL, _______, \
+  _______, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_PPLS,   KC_P1,   KC_P2,   KC_P3, KC_LPRN, KC_RPRN, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MMPR, KC_MMNX, KC_PMNS,   KC_P0,   KC_P0, KC_PDOT, KC_PEQL, _______, \
                              _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
